8014fc68904d1f54c69e26c09c7793803e822407
John McBride <jpmmcbride@gmail.com>
2019-11-09 10:45:33 -0700

    Fold complete

--- go/list-ops/listops.go
+++ go/list-ops/listops.go
@@ -3,10 +3,11 @@ package listops
 type IntList []int
 type binFunc func(int, int) int
 type predFunc func(int) bool
+type unaryFunc func(int) int
 
 func (list IntList) Foldr(f binFunc, init int) int {
-	for _, x := range list {
-		init = f(init, x)
+	for _, x := range list.Reverse() {
+		init = f(x, init)
 	}
 
 	return init
@@ -20,26 +21,43 @@ func (list IntList) Foldl(f binFunc, init int) int {
 	return init
 }
 
-func (list IntList) Filter() {
-
+func (list IntList) Filter(f predFunc) IntList {
+	return list
 }
 
-// func (*list) Length() {
+func (list IntList) Length() int {
+	return len(list)
+}
 
-// }
+func (list IntList) Map(f unaryFunc) IntList {
+	return list
+}
 
-// func (*list) Map() {
+func (list IntList) Reverse() IntList {
+	frontRunner := 0
+	backRunner := list.Length() - 1
+	var temp int
 
-// }
+	if backRunner == 0 {
+		return list
+	}
 
-// func (*list) Reverse() {
+	for frontRunner <= backRunner {
+		temp = list[frontRunner]
+		list[frontRunner] = list[backRunner]
+		list[backRunner] = temp
 
-// }
+		frontRunner++
+		backRunner--
+	}
 
-// func (*list) Append() {
+	return list
+}
 
-// }
+func (list IntList) Append(x IntList) IntList {
+	return list
+}
 
-// func (*list) Concat() {
-
-// }
+func (list IntList) Concat(x []IntList) IntList {
+	return list
+}
